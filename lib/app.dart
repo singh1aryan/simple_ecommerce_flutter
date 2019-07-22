71be4a1d6309ef62a963a15ff3110040ebf24b7f
Aryan Singh <Aryan.Singh@arrow.com>
2019-07-22 12:01:20 -0600

    remove app bar, backdrop

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:simple_ecommerce/login.dart';
 import 'home.dart';
 import 'colors.dart';
+import 'backdrop.dart';
+import 'model/product.dart';
 //import 'supplemental/cut_corners_border.dart';
 
 // TODO: Build a Shrine Theme (103)
@@ -61,7 +63,17 @@ class ShrineApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shrine',
       // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
-      home: HomePage(),
+      // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
+      home: Backdrop(
+        // TODO: Make currentCategory field take _currentCategory (104)
+        currentCategory: Category.all,
+        // TODO: Pass _currentCategory for frontLayer (104)
+        frontLayer: HomePage(),
+        // TODO: Change backLayer field value to CategoryMenuPage (104)
+        backLayer: Container(color: kShrinePink100),
+        frontTitle: Text('SHRINE'),
+        backTitle: Text('MENU'),
+    ),
       // TODO: Make currentCategory field take _currentCategory (104)
       // TODO: Pass _currentCategory for frontLayer (104)
       // TODO: Change backLayer field value to CategoryMenuPage (104)
--- lib/colors.dart
+++ lib/colors.dart
@@ -1,11 +1,24 @@
+//import 'package:flutter/material.dart';
+//
+//const kShrinePink50 = const Color(0xFFFEEAE6);
+//const kShrinePink100 = const Color(0xFFC5CAE9); //100
+//const kShrinePink300 = const Color(0xFF7986CB); //300
+//const kShrinePink400 = const Color(0xFFEAA4A4);
+//
+//const kShrineBrown900 = const Color(0xFF1A237E);//900
+//
+//const kShrineErrorRed = const Color(0xFFC5032B);
+//
+//const kShrineSurfaceWhite = const Color(0xFFFFFBFA);
+//const kShrineBackgroundWhite = Colors.white;
 import 'package:flutter/material.dart';
 
 const kShrinePink50 = const Color(0xFFFEEAE6);
-const kShrinePink100 = const Color(0xFFC5CAE9); //100
-const kShrinePink300 = const Color(0xFF7986CB); //300
+const kShrinePink100 = const Color(0xFFFEDBD0);
+const kShrinePink300 = const Color(0xFFFBB8AC);
 const kShrinePink400 = const Color(0xFFEAA4A4);
 
-const kShrineBrown900 = const Color(0xFF1A237E);//900
+const kShrineBrown900 = const Color(0xFF442B2D);
 
 const kShrineErrorRed = const Color(0xFFC5032B);
 
